d9a2afb6cb035c6df1f72bf994ccba83268c9f60
Loonaken <kendoku328@gmail.com>
2022-12-24 15:36:39 +0900

    #2 migration file新規追加

--- database/migrations/2022_12_24_053115_create_comments_table.php
+++ database/migrations/2022_12_24_053115_create_comments_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('body');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 };
