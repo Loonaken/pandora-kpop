02080475dd23da80c081193bc2c4d2bdcc4e336d
Loonaken <kendoku328@gmail.com>
2023-01-08 19:16:14 +0900

    フォームリクエスト

--- resources/views/admin/images/edit.blade.php
+++ resources/views/admin/images/edit.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                <x-input-error :messages="$errors->get('image')" class="mt-2" />
                   <form method="POST"  action="{{route('admin.images.update' , ['image'=> $image->id])}}" enctype="multipart/form-data" >
                     @csrf
                     @method('PUT')
