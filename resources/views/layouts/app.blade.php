6f0b013e6f11e1bab9be1ba2bffbff8c38ee21a6
Loonaken <kendoku328@gmail.com>
2023-01-23 10:04:00 +0900

    favicon関係の修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="あなたのハマるKpopが見つかる" >
 
-        <title>{{ config('app.name', 'Pandora K-pop') }}</title>
+        <title>{{ config('app.name', 'Pandora_Kpop') }}</title>
+
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="icon" href="/favicon.ico" id="favicon" >
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" >
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -21,6 +25,7 @@
     background: orange;
     font-weight: 500;
     color: #fff;
+            }
         </style>
     </head>
     <body class="font-sans antialiased">
