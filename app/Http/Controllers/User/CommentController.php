aec9533b06c83faf08980f501dbf41c48bd20295
Loonaken <kendoku328@gmail.com>
2023-01-25 16:01:22 +0900

    CommentController create+store実装

--- app/Http/Controllers/User/CommentController.php
+++ app/Http/Controllers/User/CommentController.php
@@ -5,6 +5,12 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Comment;
+use App\Http\Requests\CommentRequest;
+use Illuminate\Support\Facades\Auth;
+
+
+
 
 
 class CommentController extends Controller
@@ -18,17 +24,33 @@ class CommentController extends Controller
 
     public function index()
     {
-        return view('user.comments.index');
+        $comments = Comment::orderBy('created_at', 'desc')->get();
+
+        return view('user.comments.index', compact('comments'));
     }
 
     public function create()
     {
+        $user = User::where('id', Auth::id())->first();
 
+        return view ('user.comments.create', compact('user'));
     }
 
-    public function store(Request $request)
+    public function store(CommentRequest $request)
     {
-        //
+        $id = $request->user_id;
+        $user_id = (int)$id;
+
+        Comment::create([
+            'user_id'=>$user_id,
+            'title' => $request->title,
+            'body' => $request->body,
+        ]);
+
+        return redirect()
+        ->route('user.comments.index')
+        ->with(['message'=> 'コメントが登録されました。' , 'status'=>'info']);
+
     }
 
     public function show($id)
--- resources/views/admin/groups/create.blade.php
+++ resources/views/admin/groups/create.blade.php
@@ -82,10 +82,11 @@
 
                     </div>
 
-                    <div class="flex p-2 mt-8 w-full">
-                        <button type="button" onclick="location.href='{{route('admin.songs.index')}}'" class="flex mx-auto text-black bg-gray-300 border-0 py-2 px-8 focus:outline-none hover:bg-gray-200 rounded text-lg">戻る</button>
-                        <button type="submit" class="flex mx-auto text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">登録</button>
-                    </div>
+                    
+                    <div class="flex justify-around items-center p-2 w-full">
+                      <x-original.return onclick="location.href='{{route('admin.songs.index')}}'" />
+                      <x-original.action action=登録 />
+                  </div>
                 </form>
 
                 {{-- fin contents --}}
