82cbf26989a9ac0ff60e3221f8eab6751386f2fb
Loonaken <kendoku328@gmail.com>
2023-02-08 19:15:34 +0900

    userビュー調整

---
+++ resources/views/components/original/user-show.blade.php
@@ -0,0 +1,26 @@
+<a href="{{$song->youtube_link}}" target="_blank">
+  <section class="text-gray-600 body-font overflow-hidden ">
+    <div class="container mb-4 mx-auto hover:border-4 hover:border-lime-300 transition delay-100 ease-in-out  ">
+      <div class="lg:w-4/5 mx-auto flex flex-wrap items-center ">
+        <div class="basis-1/3 ">
+          <x-thumbnail :filename="$song->image->filename"  class="mb-0  " />
+          </div>
+          <div class="basis-2/3 pl-8">
+            <div class="flex items-center">
+              <p class="mr-4">曲名</p>
+              <p class="text-gray-900 text-2xl title-font font-medium">{{$song->name}}</p>
+            </div>
+            <div class="flex mt-2">
+              <p class=" mr-4">グループ名</p>
+              <p class="text-gray-900 title-font font-medium">{{$song->group->name}}</p>
+            </div>
+            <p class="leading-relaxed mt-2"> {{$song->information ?? ''}} </p>
+            <div class="flex  gap-4 mt-2">
+              <p>#{{$song->emotion->name ?? '登録なし'}}</p>
+              <p>#{{$song->period->term ?? '登録なし'}}</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</a>
--- resources/views/user/hashtags/emotion.blade.php
+++ resources/views/user/hashtags/emotion.blade.php
@@ -14,33 +14,8 @@
                 @if (!empty($songs->toArray()))
                 @foreach ($songs as $song)
 
-                {{-- song name --}}
                 <div class="my-12 border-b-2 border-gray-400 last:border-0">
-                  <div class=" w-1/2 max-w-2xl mx-auto bg-pink-400 border-0 p-2 rounded-md focus:outline-none -mt-6 mb-4">
-                    <div class="text-white text-center rounded text-3xl mb-2"> {{$song->name}}</div>
-                  </div>
-                {{-- fin song name --}}
-
-                    <section class="text-gray-600 body-font overflow-hidden ">
-                      <div class="container mb-4 py-2 mx-auto  ">
-                        <div class="lg:w-4/5 mx-auto flex flex-wrap items-center ">
-                          <div class="basis-1/3 hover:border-4 hover:border-lime-300 transition delay-100 ease-in-out">
-                            <a href="{{$song->youtube_link}}" target="_blank">
-                            <x-thumbnail :filename="$song->image->filename"  class="mb-0  " />
-                            </a>
-                          </div>
-
-                          <div class="basis-2/3 pl-8">
-                            <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{$song->group->name}}</h1>
-                            <p class="leading-relaxed"> {{$song->information ?? ''}} </p>
-                            <div class="flex gap-4 mt-4">
-                              <p>#{{$song->emotion->name ?? '登録なし'}}</p>
-                              <p>#{{$song->period->term ?? '登録なし'}}</p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </section>
+                  <x-original.user-show :song=$song /> {{--一つ一つの曲情報を表示している --}}
                 </div>
                 @endforeach
                 @endif
